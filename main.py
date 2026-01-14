041b0f5c366253fb4a666b8e6fae2e9914b06431
Oliver Lavers <oliver.f.lavers@gmail.com>
2026-01-14 19:42:03 +1000

    finished?

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def main():
     import cowsay
-    cowsay.cow("\n---\n\nSuccess! You just installed Python, set up a virtual environment, installed dependencies, and ran this project!")
+    print("\n---\n\n")
+    cowsay.cow("Success! You just installed Python, set up a virtual environment, installed dependencies, and ran this project!")
 
 if __name__ == "__main__":
     main()

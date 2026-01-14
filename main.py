47aca27e079f42265b6be230cc29a7566be04788
Oliver Lavers <oliver.f.lavers@gmail.com>
2026-01-14 19:35:20 +1000

    added success message

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 def main():
     import cowsay
-
-    cowsay.cow("test")
+    cowsay.cow("Success! You just installed Python, set up a virtual environment, installed dependencies, and ran this project!")
 
 if __name__ == "__main__":
     main()
